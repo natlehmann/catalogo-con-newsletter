55ca807a1e1c7b00e222cd8b80ff9fcdfebde658
natlehmann@gmail.com <natlehmann@gmail.com@b55e696a-5467-d2f1-746c-9c361a48dcae>
2013-06-09 22:32:24 +0000

    navegacion pagina productos

--- trunk/src/main/java/ar/com/almaDeJazmin/website/domain/ConfigConstants.java
+++ trunk/src/main/java/ar/com/almaDeJazmin/website/domain/ConfigConstants.java
@@ -8,8 +8,8 @@ public class ConfigConstants {
 	public static final int PRODUCT_GALLERY_FULL_SIZE_IMG_WIDTH = 600;
 	public static final int PRODUCT_GALLERY_FULL_SIZE_IMG_HEIGHT = 400;
 	
-	public static final int PRODUCT_GALLERY_THUMB_IMG_WIDTH = 400;
-	public static final int PRODUCT_GALLERY_THUMB_IMG_HEIGHT = 300;
+	public static final int PRODUCT_GALLERY_THUMB_IMG_WIDTH = 460;
+	public static final int PRODUCT_GALLERY_THUMB_IMG_HEIGHT = 390;
 	
 	public static final long TEXT_FILE_MAX_SIZE = 1048576L; // 1 Mb
 	public static final long IMAGE_FILE_MAX_SIZE = 1048576L; // 1 Mb
