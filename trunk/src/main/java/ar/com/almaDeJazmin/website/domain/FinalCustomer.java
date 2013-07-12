129363afe51004d9ff781c42fbfb2c735106e952
natlehmann@gmail.com <natlehmann@gmail.com@b55e696a-5467-d2f1-746c-9c361a48dcae>
2013-07-12 00:12:29 +0000

    se agrega informacion de cada contacto especifico al mail resumen

--- trunk/src/main/java/ar/com/almaDeJazmin/website/domain/FinalCustomer.java
+++ trunk/src/main/java/ar/com/almaDeJazmin/website/domain/FinalCustomer.java
@@ -8,5 +8,21 @@ import javax.persistence.Entity;
 public class FinalCustomer extends Contact {
 
 	private static final long serialVersionUID = -3092838395727011286L;
+	
+	@Override
+	public String getMailInfo() {
+		
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("Nombre: ");
+		buffer.append(this.getName());
+		buffer.append("\nMail: ");
+		buffer.append(this.getEmail());
+		buffer.append("\nTeléfono: ");
+		buffer.append(this.getPhoneNumber());
+		buffer.append("\nComentario: ");
+		buffer.append(this.getComment());
+		
+		return buffer.toString();
+	}
 
 }
